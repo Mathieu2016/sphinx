69d86bc9ec525b27ad18f1c1a4972517b569b52c
“Mathieu2016” <mathieu@vip.qq.com>
2023-06-15 23:19:13 +0800

    change into python

--- src/config.py
+++ src/config.py
@@ -2,5 +2,5 @@ from pydantic import BaseModel
 
 
 class SphinxConfig:
-    open_ai_key: str = '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'
+    open_ai_key: str = ''
     open_ai_proxy: str = 'http://127.0.0.1:15732'
